80db8dfc2ff96577d380b4c09c4352e5156e10c4
Unkn8wn69 <79060957+Unkn8wn69@users.noreply.github.com>
2023-10-18 18:58:12 +0200

    Added all variables to info

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ async def get_address(update, context):
     user_data = context.user_data
     user_data['address'] = update.message.text
 
-    await update.message.reply_text("Address saved. You can now continue.")
+    await info(update, context)
     return ConversationHandler.END
 
 async def info(update, context):
@@ -110,15 +110,38 @@ async def info(update, context):
     
     if user_info:
         info_text = f"""
+*Coin Details*
+
 Coin: {display_if_set(user_info, 'ticker_to')}
 Network: {display_if_set(user_info, 'network_to')}
 Address: `{display_if_set(user_info, 'address')}`
+Amount: `{user_info['amount'] if 'amount' in user_info else '0.1'}`
+Memo/ExtraID: `{user_info['memo'] if 'memo' in user_info else '0'}`
 
-*Design options:*
+*Transaction Type*
 
+Donation: {user_info['donation'] if 'donation' in user_info else 'False'}
+Remove Direct-Pay (Removes option to pay with receiving coin): {user_info['remove_direct_pay'] if 'remove_direct_pay' in user_info else 'False'}
+Allow to edit amount: {user_info['editable'] if 'editable' in user_info else 'False'}
+Simpler checkout: {user_info['simple_mode'] if 'simple_mode' in user_info else 'False'}
+
+*UI & Appearance*
+
+Name: {display_if_set(user_info, 'name')}
+Description: {display_if_set(user_info, 'description')}
 Button color: `{user_info['buttonbgcolor'] if 'buttonbgcolor' in user_info else 'ff3c00'}`
 Text color: `{user_info['textcolor'] if 'textcolor' in user_info else 'fffff'}`
 Widget Background: {user_info['bgcolor'] if 'bgcolor' in user_info else 'False'}
+
+*Additional*
+
+Default coin: {user_info['ticker_from'] if 'ticker_from' in user_info else 'BTC'}
+Default network: {user_info['network_from'] if 'network_from' in user_info else 'Mainnet'}
+Trocador referral: {display_if_set(user_info, 'referral')}
+Fiat equivalent: {display_if_set(user_info, 'fiat_equiv')}
+Notification email: {display_if_set(user_info, 'email')}
+Minimum KYC score (A, B or C): {display_if_set(user_info, 'min_logpolicy')}
+Webhook: {display_if_set(user_info, 'webhook')}
 
 Link: `{generate_link(user_info)}`
 """
